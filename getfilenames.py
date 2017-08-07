d3414dc75957031aa3283b04f41ebd3680e717c4
s6m1z5 <shimizuryota3@gmail.com>
2017-08-07 12:13:21 +0900

    Update getfilenames.py

--- getfilenames.py
+++ getfilenames.py
@@ -1,7 +1,9 @@
 #coding:utf-8
+#python2.7
+#現在のディレクトリに存在するファイル名を取得する
 
 import numpy as np
 import glob
 
-a = glob.glob("./*.jpg")
+a = glob.glob("./*")#*.jpgのように拡張子を指定できる, *のみにすればすべてのファイル名を抽出
 np.savetxt("filenames.csv", np.array(a), delimiter=",", fmt="%s")
