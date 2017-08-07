af1c133cfff79379596b94a40902b0ec0303766b
s6m1z5 <shimizuryota3@gmail.com>
2017-08-07 11:58:19 +0900

    Update imagestack.py

--- imagestack.py
+++ imagestack.py
@@ -1,30 +1,19 @@
 #coding:utf-8
+#python2.7
 #画像を縦に並べる
 
 import numpy as np
 from PIL import Image
+FILENAME = "names.csv"#画像ファイル名が,区切りで縦に並べられたファイル
 
 def loadimage(imagename):#画像を開く関数
 	img = Image.open(imagename)#画像の読み込み
 	img = np.asarray(img).astype(np.float32)#読み込んだ画像をnumpy配列として保存
 	return img
 
-def img_stack(img1, img2):
-	img = np.vstack((img1, img2))
-	return img
-
 if __name__ == "__main__":
-	data = np.loadtxt("result.txt", delimiter="\n", dtype="string")
-	images = map(loadimage, data)
-	v = np.vstack(images)
+	data = np.loadtxt(FILENAME, delimiter=",", dtype="string")
+	images = map(loadimage, data)#画像をすべて開く
+	v = np.vstack(images)#画像を縦に並べる
 	Image.fromarray(np.uint8(v)).save("stkimage.png")
-	"""check = True
-	for x in data:
-		img = loadimage(x)
-		print(x)
-		if check == True:
-			v = img
-			check = False
-		else:
-			v = img_stack(v, img)
-	Image.fromarray(np.uint8(v)).save("stkimage.png")"""
+	print("img saved.")
